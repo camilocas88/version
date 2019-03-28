01a28dc8880f7123ddecf24c2fa7ae85a161a2cc
Camilo Castillo <34112917+camilocas88@users.noreply.github.com>
2019-03-27 22:25:10 -0500

    logina page

--- extend/header.php
+++ extend/header.php
@@ -6,32 +6,38 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="../css/main.css">
+    <link rel="stylesheet" href="../css/flaticon/flaticon.css">
     <link href="../css/fontawesome/css/all.css" rel="stylesheet">
-    <!-- <link rel="stylesheet" href="css/bootstrap.min.css"> -->
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <title>La Licorera 2.0</title>
 </head>
 
 <body>
-    <header>
+    <header class="header">
         <div class="loader">
             <div class="load"><img src="../img/loader/loader.gif" alt=""></div>
         </div>
         <nav class="bgnav">
-            <a href="" class="bgnav_space"></a><span class="bgnav_span"></span><a href=""><i class="fal fa-phone"></i> (1) 756 0060</a><span class="bgnav_span"></span>
-            <a href=""><i class="fal fa-phone"></i> 300 203 5430</a><span class="bgnav_span"></span>
+            <a href="" class="bgnav_space"></a><span class="bgnav_span"></span><a href=""><i class="flaticon-phone-call fal"></i> (1) 756 0060</a><span class="bgnav_span"></span>
+            <a href=""><i class="flaticon-phone-call fal"></i> 300 203 5430</a><span class="bgnav_span"></span>
             <a href=""><i class="fab fa-whatsapp"></i> 304 484 8594</a><span class="bgnav_span"></span><a class="bgnav_space" href=""></a>
         </nav>
 
         <nav class="menu">
             <div class="menu-left">
                 <ul>
-                    <li class="open-slide"><a onclick="openSlideMenu()"><i class="fal fa-bars fa-2x"></i><span class="menu_title">Menú</span></a></li>
-                    <li><a href="extend/login.php"><i class="fal fa-user-circle"></i></a></li>
-                    <li><a href="#"><i class="fal fa-search fa-2x"></i><span class="menu_title--left">Buscar</span></a></li>
+                    <li class="open-slide"><a onclick="openSlideMenu('side-menu')">
+                    <div class="menu-btn">
+                        <div class="btn-line"></div>
+                        <div class="btn-line"></div>
+                        <div class="btn-line"></div>
+                    </div>
+                    <span class="titleMenu">Menú</span></a></li>
+                    <li class="bar_li"><div class="bar"></div></li>
+                    <li><a href="extend/login.php"><i class="fal fa-user-circle _user"></i></a></li>
+                    <li><a href="#"><i class="fal fa-search fa-2x"></i><span class="menu_title">Buscar</span></a></li>
                 </ul>
             </div>
-            <a href="../index.php">
+            <a href="/version">
                 <picture>
                     <source class="licorera_logo" type="../img/logos/logo.svg" srcset="pyramid.svg">
                     <source class="licorera_logo" type="../img/logos/logo.webp" srcset="pyramid.webp">
@@ -40,10 +46,11 @@
             </a>
             <div class="menu-right">
                 <ul>
-
-                    <li><a href="#"><i class="far fa-search fa-2x --left"></i><span class="menu_title--left">Buscar</span></a></li>
-                    <li><a href="#"><i class="far fa-clock fa-2x --left"></i><span class="menu_title--left">Tiempo</span></a></li>
-                    <li><a href="#"><i class="far fa-shopping-cart fa-2x --left"></i><span class="menu_title--left">Carrito</span></a></li>
+                    <li><a href="#"><span class="menu_title--left">Buscar</span><i class="fal fa-search"></i></a></li>
+                    <li class="bar_li"><div class="bar"></div></li>
+                    <li><a href="#"><span class="menu_title--left">Tiempo</span><i class="fal fa-clock"></i><span class="menu_title--right">Tiempo</span></a></li>
+                    <li class="bar_li"><div class="bar"></div></li>                    
+                    <li><a href="#"><span class="menu_title--left">Carrito</span><i class="fal fa-shopping-cart"></i><span class="menu_title--right">Carrito</span></a></li>
                 </ul>
 
             </div>
@@ -53,29 +60,36 @@
         <div id="side-menu" class="side-nav">
             <!-- Location Menu -->
             <ul class="side-menu-ul location">
-                <li class="location"><a href="#"><i class="fal 
-                fa-map-marker-alt"></i> Ciudad <i class="fal fa-chevron-right"></i></a>
-                    <ul>
-                        <li><a href="#">Bucaramanga</a></li>
-                        <li><a href="#">Cartagena</a></li>
-                        <li><a href="#">Cali</a></li>
-                        <li><a href="#">Medellín</a></li>
-                        <li><a href="#">Santa Marta</a></li>
+                <li class="location"><a href="#"><i class="flaticon-maps-and-flags"></i> Ciudad <i class="fal fa-chevron-down"></i></a>
+                    <ul class="location-block">
+                        <li><a class="-first" href="#">Bucaramanga</a><div class="line-btn"></div></li>
+                        <li><a class="-first" href="#">Cartagena</a><div class="line-btn"></div></li>
+                        <li><a class="-first" href="#">Cali</a><div class="line-btn"></div></li>
+                        <li><a class="-first" href="#">Medellín</a><div class="line-btn"></div></li>
+                        <li><a class="-last" href="#">Santa Marta</a><div class="last-btn"></div></li>
                     </ul>
                 </li>
             </ul>
 
             <!-- Login menu -->
             <ul class="side-menu__login">
-                <li><a class="btn-close" onclick="closeSlideMenu()"><i class="fal fa-bars fa-2x"></i>Menú</a></li>
-                <li><a href="#"><i class="fal fa-cocktail"></i></a></li>
-                <li>Hola Camilo Mi asdasdasdasCuenta Cerrar Cesion</li>
+            <li class="open-slide"><a class="btn_menu" onclick="closeSlideMenu('side-menu')">
+                    <div class="menu-btn side">
+                        <div class="btn-line side"></div>
+                        <div class="btn-line side"></div>
+                        <div class="btn-line side"></div>
+                    </div>
+                    <span class="menu_title">Menú</span></a></li>
+                <li class="bar_li"><div class="bar"></div></li>
+
+                <li><a href="#" class="btn-close"><i class="fal fa-user-circle"></i></a></li>
+                <li>Bienvenido <br>Ingresa a tu Cuenta Cerrar Cesion</li>
             </ul>
 
-            <ul>
+            <ul class="content_menu">
                 <!-- ariculos -->
-                <li class="side-menu-ul"><a href="#"><i class="fal fa-list-ul"></i> Productos <i class="fal fa-chevron-right"></i></a>
-                    <ul>
+                <li class="side-menu-ul"><a class="line_buttom" href="#"><i class="fal fa-list-ul"></i> Productos <i class="fal fa-chevron-down"></i></a>
+                    <ul class="products">
                         <li><a class="list" href="">Aguardiente</a></li>
                         <li><a class="list" href="">Brandy-Cognac</a></li>
                         <li><a class="list" href="">Cerveza</a></li>
@@ -90,27 +104,26 @@
                         <li><a class="list" href="">Vino</a></li>
                         <li><a class="list" href="">Vodka</a></li>
                         <li><a class="list" href="">Whisky</a></li>
-                        <hr>
+                        <hr class="middle_hr">
                         <li><a class="list --right" href="">No Alcohólicas</a></li>
                         <li><a class="list --right" href="">Otros Productos</a></li>
                         <li><a class="list --right" href="">Utensilios</a></li>
                         <li><a class="list --right" href="">Snaks</a></li>
                         <li><a class="list --right" href="">Cigarrillos</a></li>
                     </ul>
-                <li><a href="#"><i class="fal fa-thumbs-up"></i> Experiencias <i class="fal fa-chevron-right"></i> </a></li>
-                <li><a href="#"><i class="fas fa-glass-martini"></i> Recetas de Cócteles <i class="fal fa-chevron-right"></i> </a></li>
-                <li><a href="#"><i class="fal fa-gift"></i> Regalos <i class="fal fa-chevron-right"></i> </a></li>
-                <li><a href="#"><i class="fal fa-tags"></i> Promociones <i class="fal fa-chevron-right"></i> </a></li>
-                <li><a href="#"><i class="fal fa-envelope"></i> Contacto <i class="fal fa-chevron-right"></i> </a></li>
+                <li><a class="line_buttom" href="#"><i class="fal fa-thumbs-up"></i> Experiencias <i class="fal fa-chevron-right"></i> </a></li>
+                <li><a class="line_buttom" href="#"><i class="flaticon-glass"></i> Recetas de Cócteles <i class="fal fa-chevron-right"></i> </a></li>
+                <li><a class="line_buttom" href="#"><i class="fal fa-gift"></i> Regalos <i class="fal fa-chevron-right"></i> </a></li>
+                <li><a class="line_buttom" href="#"><i class="flaticon-tag"></i> Promociones <i class="fal fa-chevron-right"></i> </a></li>
+                <li><a class="line_buttom" href="#"><i class="flaticon-email"></i> Contacto <i class="fal fa-chevron-right"></i> </a></li>
             </ul>
 
-            <ul class="side-menu__bottom">
-                <li><a href="#"><i class="fal fa-question-circle fa-2x"></i> Preguntas Frecuentes</a></li>
-                <li><a href="#"><i class="fal fa-address-card fa-2x"></i> Políticas</a></li>
+            <ul class="side-menu_bottom">
+                <li class="menu_bottom--li"><a href="#"><i class="flaticon-comment"></i> Preguntas Frecuentes</a></li>
+                <li class="bar_li "><div class="bar"></div></li> 
+                <li class="menu_bottom--li"><a href="#"><i class="fal fa-address-card"></i> Políticas</a></li>
             </ul>
         </div>
     </header>
 
     <main class=container>
-
-    
--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/flaticon/flaticon.css">
     <link href="css/fontawesome/css/all.css" rel="stylesheet">
     <title>La Licorera 2.0</title>
 </head>
@@ -16,8 +17,8 @@
             <div class="load"><img src="img/loader/loader.gif" alt=""></div>
         </div>
         <nav class="bgnav">
-            <a href="" class="bgnav_space"></a><span class="bgnav_span"></span><a href=""><i class="fal fa-phone"></i> (1) 756 0060</a><span class="bgnav_span"></span>
-            <a href=""><i class="fal fa-phone"></i> 300 203 5430</a><span class="bgnav_span"></span>
+            <a href="" class="bgnav_space"></a><span class="bgnav_span"></span><a href=""><i class="flaticon-phone-call fal"></i> (1) 756 0060</a><span class="bgnav_span"></span>
+            <a href=""><i class="flaticon-phone-call fal"></i> 300 203 5430</a><span class="bgnav_span"></span>
             <a href=""><i class="fab fa-whatsapp"></i> 304 484 8594</a><span class="bgnav_span"></span><a class="bgnav_space" href=""></a>
         </nav>
 
@@ -59,8 +60,7 @@
         <div id="side-menu" class="side-nav">
             <!-- Location Menu -->
             <ul class="side-menu-ul location">
-                <li class="location"><a href="#"><i class="fal 
-                fa-map-marker-alt"></i> Ciudad <i class="fal fa-chevron-down"></i></a>
+                <li class="location"><a href="#"><i class="flaticon-maps-and-flags"></i> Ciudad <i class="fal fa-chevron-down"></i></a>
                     <ul class="location-block">
                         <li><a class="-first" href="#">Bucaramanga</a><div class="line-btn"></div></li>
                         <li><a class="-first" href="#">Cartagena</a><div class="line-btn"></div></li>
@@ -112,14 +112,14 @@
                         <li><a class="list --right" href="">Cigarrillos</a></li>
                     </ul>
                 <li><a class="line_buttom" href="#"><i class="fal fa-thumbs-up"></i> Experiencias <i class="fal fa-chevron-right"></i> </a></li>
-                <li><a class="line_buttom" href="#"><i class="fal fa-glass-martini"></i> Recetas de Cócteles <i class="fal fa-chevron-right"></i> </a></li>
+                <li><a class="line_buttom" href="#"><i class="flaticon-glass"></i> Recetas de Cócteles <i class="fal fa-chevron-right"></i> </a></li>
                 <li><a class="line_buttom" href="#"><i class="fal fa-gift"></i> Regalos <i class="fal fa-chevron-right"></i> </a></li>
-                <li><a class="line_buttom" href="#"><i class="fal fa-tags"></i> Promociones <i class="fal fa-chevron-right"></i> </a></li>
-                <li><a class="line_buttom" href="#"><i class="fal fa-envelope"></i> Contacto <i class="fal fa-chevron-right"></i> </a></li>
+                <li><a class="line_buttom" href="#"><i class="flaticon-tag"></i> Promociones <i class="fal fa-chevron-right"></i> </a></li>
+                <li><a class="line_buttom" href="#"><i class="flaticon-email"></i> Contacto <i class="fal fa-chevron-right"></i> </a></li>
             </ul>
 
             <ul class="side-menu_bottom">
-                <li class="menu_bottom--li"><a href="#"><i class="fal fa-question-circle"></i> Preguntas Frecuentes</a></li>
+                <li class="menu_bottom--li"><a href="#"><i class="flaticon-comment"></i> Preguntas Frecuentes</a></li>
                 <li class="bar_li "><div class="bar"></div></li> 
                 <li class="menu_bottom--li"><a href="#"><i class="fal fa-address-card"></i> Políticas</a></li>
             </ul>
@@ -131,7 +131,9 @@
             <img src="img/Banner1.jpg" style="width:100%" alt="">
         </section>
         <section class="banner_bottom"`>
-            <h1>Lorem, ipsum dolor.</h1>
+            <a href=""><i class="flaticon-credit-card"></i>Pago contra entrega</a>
+            <a href="">Pago seguro online</a>
+            <a href="">Recetas cocteles</a>
         </section>
 
         <h1>Contenido</h1>
